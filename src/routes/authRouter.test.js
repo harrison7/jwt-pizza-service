64418851d4e4f15249916ce669eb10872c78ebc2
harrison7 <harrison@thecaspers.net>
2024-10-01 19:03:38 -0600

    Vastly improve authRouter test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,31 +2,48 @@ const request = require('supertest');
 const app = require('../service');
 const { Role, DB } = require('../database/database.js');
 
-const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
+const testUsers = [
+  { name: 'login', email: 'reg1@test.com', password: 'a' },
+  { name: 'logout', email: 'reg2@test.com', password: 'a' },
+  { name: 'updateUser', email: 'reg3@test.com', password: 'a' },
+  { name: 'updateUser unauthorized', email: 'reg4@test.com', password: 'a' },
+  { name: 'updateUser self', email: 'reg5@test.com', password: 'a' }
+];
+let testUserAuthToken = new Array(4);
+let testUserId = new Array(4);
+const adminUser = { name: '常用名字', email: 'a@jwt.com', password: 'admin' };
+let testAdminAuthToken;
 
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
   jest.setTimeout(60 * 1000 * 5); // 5 minutes
 }
 
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = Math.random().toString(36).substring(2, 12);
-  user.email = user.name + '@admin.com';
-
-  await DB.addUser(user);
-
-  user.password = 'toomanysecrets';
-  return user;
-}
-
-const adminUser = { name: '常用名字', email: 'a@jwt.com', password: 'admin' };
-let testAdminAuthToken;
-
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
+  for (let i = 0; i < testUsers.length; i++) {
+    testUsers[i].email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const registerRes = await request(app).post('/api/auth').send(testUsers[i]);
+    testUserAuthToken[i] = registerRes.body.token;
+    testUserId[i] = registerRes.body.user.id;
+  }
+});
+
+afterAll(async () => {
+  for (let i = 0; i < testUsers.length; i++) {
+    const connection = await DB.getConnection();
+    try {
+      await connection.beginTransaction();
+      try {
+        await DB.query(connection, `DELETE FROM userRole WHERE userId = ?`, [testUserId[i]]);
+        await DB.query(connection, `DELETE FROM user WHERE id = ?`, [testUserId[i]]);
+        await connection.commit();
+      } catch {
+        await connection.rollback();
+        throw new StatusCodeError('unable to delete user', 500);
+      }
+    } finally {
+      connection.end();
+    }
+  }
 });
 
 test('register fail', async () => {
@@ -37,31 +54,31 @@ test('register fail', async () => {
 });
 
 test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  testUserAuthToken = loginRes.body.token;
+  const loginRes = await request(app).put('/api/auth').send(testUsers[0]);
+  testUserAuthToken[0] = loginRes.body.token;
   expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  //expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  const { password, ...user } = { ...testUsers[0], roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
-  await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken[0]}`);
 });
 
 test('logout', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  testUserAuthToken = loginRes.body.token;
+  const loginRes = await request(app).put('/api/auth').send(testUsers[1]);
+  testUserAuthToken[1] = loginRes.body.token;
   
-  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken[1]}`);
   expect(logoutRes.status).toBe(200);
   expect(logoutRes.body.message).toBe('logout successful');
 });
 
-test('updateUser', async () => {
+test('updateUser admin', async () => {
   const loginRes = await request(app).put('/api/auth').send(adminUser);
   testAdminAuthToken = loginRes.body.token;
   const updateUserData = { email: 'newemail@test.com', password: 'newpassword' };
   
-  const updateRes = await request(app).put(`/api/auth/3`).send(updateUserData).set('Authorization', `Bearer ${testAdminAuthToken}`);
+  const updateRes = await request(app).put(`/api/auth/${testUserId[2]}`).send(updateUserData).set('Authorization', `Bearer ${testAdminAuthToken}`);
 
   expect(updateRes.status).toBe(200);
   expect(updateRes.body.email).toBe(updateUserData.email);
@@ -69,15 +86,29 @@ test('updateUser', async () => {
 });
 
 test('updateUser unauthorized', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  testUserAuthToken = loginRes.body.token;
+  const loginRes = await request(app).put('/api/auth').send(testUsers[3]);
+  testUserAuthToken[3] = loginRes.body.token;
 
   const originalEmail = loginRes.body.email;
-  const updateUserData = { email: 'unauthorized@test.com', password: 'newpassword' };
+  const updateUserData = { email: 'unauthorized@test.com', password: 'badpassword' };
   
-  const updateRes = await request(app).put(`/api/auth/2`).send(updateUserData).set('Authorization', `Bearer ${testUserAuthToken}`);
+  const updateRes = await request(app).put(`/api/auth/${testUserId[2]}`).send(updateUserData).set('Authorization', `Bearer ${testUserAuthToken[3]}`);
 
   expect(updateRes.status).toBe(403);
   expect(updateRes.body.email).toBe(originalEmail);
-  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken[3]}`);
+});
+
+test('updateUser self', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUsers[4]);
+  testUserAuthToken[3] = loginRes.body.token;
+
+  const originalEmail = loginRes.body.email;
+  const updateUserData = { email: 'unauthorized@test.com', password: 'badpassword' };
+  
+  const updateRes = await request(app).put(`/api/auth/${testUserId[4]}`).send(updateUserData).set('Authorization', `Bearer ${testUserAuthToken[3]}`);
+
+  expect(updateRes.status).toBe(200);
+  expect(updateRes.body.email).toBe(updateUserData.email);
+  await request(app).delete('/api/auth').set('Authorization', `Bearer ${testAdminAuthToken}`);
 });
--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,37 +1,48 @@
 const request = require('supertest');
 const app = require('../service');
 const { Role, DB } = require('../database/database.js');
-const bcrypt = require('bcrypt');
 
-const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
-
-if (process.env.VSCODE_INSPECTOR_OPTIONS) {
-    jest.setTimeout(60 * 1000 * 5); // 5 minutes
-}
-
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = Math.random().toString(36).substring(2, 12);
-  user.email = user.name + '@admin.com';
-
-  await DB.addUser(user);
-
-  user.password = await bcrypt.hash('toomanysecrets', 10);
-  return user;
-}
-
+const testUsers = [
+  { name: 'login', email: 'reg1@test.com', password: 'a' },
+  { name: 'logout', email: 'reg2@test.com', password: 'a' },
+  { name: 'updateUser', email: 'reg3@test.com', password: 'a' },
+  { name: 'updateUser unauthorized', email: 'reg4@test.com', password: 'a' }
+];
+let testUserAuthToken = new Array(4);
+let testUserId = new Array(4);
 const adminUser = { name: '常用名字', email: 'a@jwt.com', password: 'admin' };
 let testAdminAuthToken;
 
-beforeAll(async () => {
-    // testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    // const registerRes = await request(app).post('/api/auth').send(testUser);
-    // testUserAuthToken = registerRes.body.token;
-    // await request(app).put('/api/auth').send(testUser);
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
 
-    const adminLoginRes = await request(app).put('/api/auth').send(adminUser);
-    testAdminAuthToken = adminLoginRes.body.token;
+beforeAll(async () => {
+  for (let i = 0; i < testUsers.length; i++) {
+    testUsers[i].email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const registerRes = await request(app).post('/api/auth').send(testUsers[i]);
+    testUserAuthToken[i] = registerRes.body.token;
+    testUserId[i] = registerRes.body.user.id;
+  }
+});
+
+afterAll(async () => {
+  for (let i = 0; i < testUsers.length; i++) {
+    const connection = await DB.getConnection();
+    try {
+      await connection.beginTransaction();
+      try {
+        await DB.query(connection, `DELETE FROM userRole WHERE userId = ?`, [testUserId[i]]);
+        await DB.query(connection, `DELETE FROM user WHERE id = ?`, [testUserId[i]]);
+        await connection.commit();
+      } catch {
+        await connection.rollback();
+        throw new StatusCodeError('unable to delete user', 500);
+      }
+    } finally {
+      connection.end();
+    }
+  }
 });
 
 test('getMenu', async () => {
